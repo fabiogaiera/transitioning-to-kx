97ecd2ed72c6742ce18efbdba77e60812dd57d6b
fabiogaiera <fabio.gaiera@outlook.com>
2025-08-24 07:52:19 +0200

    Added stuff

--- historical_database/query_engine.py
+++ historical_database/query_engine.py
@@ -3,7 +3,16 @@ import pykx as kx
 
 def run_query(database_path):
     db = kx.DB(path=database_path)
-    trades1 = db.table.trade_data.select(where= kx.Column('date') == kx.q('2025.04.02'))
-    trades2 = db.table.trade_data.select(where= kx.Column('date') == kx.q('2025.04.03'))
-    print(trades1)
-    print(trades2)
+    trades = db.table.trade_data.select(
+
+        columns={'close': kx.Column('price').last()},
+
+        where=(
+                (kx.Column('date') >= kx.q('2025.04.02')) &
+                (kx.Column('date') <= kx.q('2025.04.09'))
+        ),
+
+        by=kx.Column('date')
+
+    )
+    print(trades)
--- historical_database/volatility_script.py
+++ historical_database/volatility_script.py
@@ -1,4 +1,3 @@
-from historical_database.historical_database_creator import create_or_update_database
 from historical_database.query_engine import run_query
 
 if __name__ == "__main__":
@@ -7,13 +6,13 @@ if __name__ == "__main__":
     #        sys.exit(1)
 
     # Path to the CSV file
-    trades_data = "/home/fabio/data/2025.04.03_AAPL_trades.csv"
+    trades_data = "/home/fabio/data/2025.04.09_AAPL_trades.csv"
     database_path = "/home/fabio/db"
 
     # Daylight Saving Time (DST) for Eastern Time (ET) in the U.S. as we're analyzing the IBM ticker.
     # Timestamp data type in q
-    market_open = '2025.04.03D13:30:00.000000000'
-    market_close = '2025.04.03D20:00:00.000000000'
+    market_open = '2025.04.09D13:30:00.000000000'
+    market_close = '2025.04.09D20:00:00.000000000'
 
-    #create_or_update_database(trades_data, database_path, market_open, market_close)
+    # create_or_update_database(trades_data, database_path, market_open, market_close)
     run_query(database_path)
